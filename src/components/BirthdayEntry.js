9cdf50f4994f3405a196c828db91f48511888ccb
Eric <110496410+EricChee8@users.noreply.github.com>
2023-09-06 16:10:03 +0100

    implemented inputs and css

--- src/components/BirthdayEntry.js
+++ src/components/BirthdayEntry.js
@@ -1,5 +1,23 @@
 const BirthdayEntry = () => {
-  return <div className="birthday-entry-container">BirthdayEntry</div>;
+  return <div className="birthday-entry-container">
+    <form className="birthday-entry">
+      <div className="birthday-inputs">
+        <div className="day-input">
+          <label>DAY</label>
+          <input type="text" placeholder="DD"/>
+        </div>
+        <div className="month-input">
+          <label>MONTH</label>
+          <input type="text" placeholder="MM"/>
+        </div>
+        <div className="year-input">
+          <label>YEAR</label>
+          <input type="text" placeholder="YYYY"/>
+        </div>
+      </div>
+      <hr />
+    </form>
+  </div>;
 };
 
 export default BirthdayEntry;
