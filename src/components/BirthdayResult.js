5f9d5e5483dd47e6e55c9c6383feb4392ce3c9b8
Eric Chee <ericchee@hotmail.co.uk>
2023-10-10 01:23:21 +0100

    update css

--- src/components/BirthdayResult.js
+++ src/components/BirthdayResult.js
@@ -1,9 +1,9 @@
 const BirthdayResult = () => {
   return (
     <div className="birthday-result-container">
-      <p className="years">38 years</p>
-      <p className="months">3 months</p>
-      <p className="days">2 days</p>
+      <p className="years">- - years</p>
+      <p className="months">- - months</p>
+      <p className="days">- - days</p>
     </div>
   );
 };
