e0b641eb326b23d45aebb8a1be05bde59e24351e
Eric Chee <ericchee@hotmail.co.uk>
2023-10-13 23:17:39 +0100

    implement logic number inputs to reflect on the results page

--- src/components/BirthdayEntry.js
+++ src/components/BirthdayEntry.js
@@ -1,30 +1,71 @@
 import iconArrow from "../assets/images/icon-arrow.svg";
+import { useState } from 'react';
 
-const BirthdayEntry = () => {
-  return <div className="birthday-entry-container">
-    <form className="birthday-entry">
-      <div className="birthday-inputs">
-        <div className="day-input">
-          <label>DAY</label>
-          <input type="text" placeholder="DD"/>
+const BirthdayEntry = ({updateSharedMonth, updateSharedDay }) => {
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
+  
+  const handleMonthChange = (event) => {
+    const selectedMonth = event.target.value;
+    setMonth(selectedMonth); 
+
+    const dayInput = document.getElementById("dayInput");
+    if (selectedMonth === "02") {
+      dayInput.setAttribute("max", "28");
+    } else if (["04", "06", "09", "11"].includes(selectedMonth)) {
+      dayInput.setAttribute("max", "30");
+    } else {
+      dayInput.setAttribute("max", "31");
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateSharedMonth(month);
+    updateSharedDay(day);
+  };
+
+  return (
+    <div className="birthday-entry-container">
+      <form className="birthday-entry">
+        <div className="birthday-inputs">
+          <div className="day-input">
+            <label>DAY</label>
+            <input
+              id="dayInput"
+              type="number"
+              placeholder="DD"
+              min="1"
+              max="31"
+              value={day} 
+              onChange={(event) => setDay(event.target.value)} 
+            />
+          </div>
+          <div className="month-input">
+            <label>MONTH</label>
+            <input
+              type="number"
+              placeholder="MM"
+              min="1"
+              max="12"
+              value={month} 
+              onChange={handleMonthChange}
+            />
+          </div>
+          <div className="year-input">
+            <label>YEAR</label>
+            <input type="number" placeholder="YYYY" />
+          </div>
         </div>
-        <div className="month-input">
-          <label>MONTH</label>
-          <input type="text" placeholder="MM"/>
+        <div className="button-container">
+          <hr />
+          <div className="submit-button">
+            <img onClick={handleSubmit} className="icon-arrow" src={iconArrow} alt="" />
+          </div>
         </div>
-        <div className="year-input">
-          <label>YEAR</label>
-          <input type="text" placeholder="YYYY"/>
-        </div>
-      </div>
-      <div className="button-container">
-        <hr />
-        <div className="submit-button">
-          <img classnName="icon-arrow" src={iconArrow} alt="" />
-        </div>
-      </div>
-    </form>
-  </div>;
+      </form>
+    </div>
+  );
 };
 
 export default BirthdayEntry;
--- src/components/BirthdayResult.js
+++ src/components/BirthdayResult.js
@@ -1,9 +1,36 @@
-const BirthdayResult = () => {
+const BirthdayResult = ({month, day}) => {
+
+  const cleanSharedDay = (day) => {
+    // Remove leading '0' if present
+    if (day.length > 1 && day[0] === '0') {
+      return day.slice(1);
+    }
+    return day;
+  };
+  
+  const cleanSharedMonth = (month) => {
+    // Remove leading '0' if present
+    if (month.length > 1 && month[0] === '0') {
+      const correctedMonth = parseInt(month.slice(1)) - 1
+      return correctedMonth;
+    }
+    return month;
+  };
+
+  const birthday = new Date(1997, cleanSharedMonth(month), cleanSharedDay(day)); // Change this to your actual birthday
+
+  const currentDate = new Date();
+  const timeDifference = currentDate - birthday;
+
+  const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365.25));
+  const months = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
+  const days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 30.44) / (1000 * 60 * 60 * 24)));
+
   return (
     <div className="birthday-result-container">
-      <p className="years">- - years</p>
-      <p className="months">- - months</p>
-      <p className="days">- - days</p>
+      <p className="years">{years} years</p>
+      <p className="months">{months} months</p>
+      <p className="days">{days} days</p>
     </div>
   );
 };
