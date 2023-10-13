700ba0222e17d905f619b8a32f6d1f88711a680f
Eric Chee <ericchee@hotmail.co.uk>
2023-10-13 23:17:03 +0100

    added shared variables for month and day

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
 import BirthdayEntry from "./components/BirthdayEntry";
 import BirthdayResult from "./components/BirthdayResult";
+import { useState } from 'react';
 
 function App() {
+  const [month, setSharedMonth] = useState('');
+  const [day, setSharedDay] = useState('');
+
+  const updateSharedMonth = (newValue) => {
+    setSharedMonth(newValue);
+  };
+
+  const updateSharedDay = (newValue) => {
+    setSharedDay(newValue);
+  };
+
   return (
     <div className="container">
       <div className="top-row">
-        <BirthdayEntry />
+        <BirthdayEntry 
+         updateSharedMonth={updateSharedMonth} 
+         updateSharedDay={updateSharedDay}/>
       </div>
       <div className="bottom-row">
-        <BirthdayResult />
+        <BirthdayResult 
+        month={month} 
+        day={day}/>
       </div>
     </div>
   );
